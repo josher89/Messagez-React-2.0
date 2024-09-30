de0b8a9831981e71b6f8e80d2d20454e27aedaa1
Josher89 <j.hernandezruiz89@gmail.com>
2024-09-30 10:44:52 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [messagez, setMessagez] = useState([]);
+  const [newMessagez, setNewMessagez] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/messagez/')
+      .then(response => {
+        setMessagez(response.data);
+      })
+      .catch(error => console.error("There was an error fetching the messagez", error));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newMessagez.trim() !== "") {
+      axios.post('http://localhost:8000/api/messagez/', { text: newMessagez })
+        .then(response => {
+          setMessagez([response.data, ...messagez]);
+          setNewMessagez("");
+        })
+        .catch(error => console.error("There was an error posting the messagez", error));
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Messagez</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newMessagez}
+          onChange={e => setNewMessagez(e.target.value)}
+          placeholder="Write a messagez"
+        />
+        <button type="submit">Send messagez</button>
+      </form>
+
+      <ul>
+        {messagez.map((messagez) => (
+          <li key={messagez.id}>
+            {messagez.text} - {new Date(messagez.timestamp).toLocaleString()}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
